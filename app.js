4c52618bd3497f63209a4a88e2a327529b6414e6
toerikv <toerikv@gmail.com>
2022-04-01 12:48:22 -0600

    week5

---
+++ app.js
@@ -0,0 +1,5 @@
+$(".navItem").css("color","white");
+$("#nav").css("background-color","#145");
+$('ul li:even').css("background-color","#ccc");
+$('ul li:odd').css("background-color","#aaa");
+$('#myInput').val('Erik');
